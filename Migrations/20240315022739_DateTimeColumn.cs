be99d8baef1676521ec7600283c135f9658e02ec
vothuc2k3 <vothuc2k3@gmail.com>
2024-03-15 09:29:24 +0700

    Change submissionDate type to DateTime

--- Migrations/20240315022739_DateTimeColumn.cs
+++ Migrations/20240315022739_DateTimeColumn.cs
@@ -1,15 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace COMP1640.Migrations
 {
     /// <inheritdoc />
-    public partial class ProfileImagePathCanBeNull : Migration
+    public partial class DateTimeColumn : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "submissionDate",
+                table: "Contributions",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
             migrationBuilder.AlterColumn<string>(
                 name: "ProfileImagePath",
                 table: "AspNetUsers",
@@ -24,6 +33,14 @@ namespace COMP1640.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "submissionDate",
+                table: "Contributions",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
             migrationBuilder.AlterColumn<string>(
                 name: "ProfileImagePath",
                 table: "AspNetUsers",
